e2e0756a4a56a98097023d49bd38b819fa8a79b1
Ricardo Tavares <rjtavares@gmail.com>
2025-03-30 11:37:37 +0100

    created task_actions table

--- schema.sql
+++ schema.sql
@@ -1,8 +1,17 @@
 DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS task_actions;
 
 CREATE TABLE tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     description TEXT NOT NULL,
     due_date DATE NOT NULL,
     completed BOOLEAN NOT NULL DEFAULT 0 -- 0 for false, 1 for true
+);
+
+CREATE TABLE task_actions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task_id INTEGER NOT NULL,
+    action_description TEXT NOT NULL,
+    action_date DATE NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
