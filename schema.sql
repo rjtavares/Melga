026e2a0eba67f5790a11160ff3c5925aed14beb3
Ricardo Tavares <rjtavares@gmail.com>
2025-06-28 12:11:49 +0100

    feat: Update database schema and ignore backups
    - Update the database schema to include new tables and columns.
    - Create a migration script to move data to the new schema.
    - Update .gitignore to exclude database backups and migration scripts.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,25 @@
 DROP TABLE IF EXISTS tasks;
 DROP TABLE IF EXISTS task_actions;
+DROP TABLE IF EXISTS goals;
+DROP TABLE IF EXISTS notes;
+DROP TABLE IF EXISTS random_things_to_do;
+DROP TABLE IF EXISTS habits;
+
+CREATE TABLE goals (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT NOT NULL,
+        created_date DATE NOT NULL,
+        target_date DATE NOT NULL,
+        completed BOOLEAN NOT NULL DEFAULT 0, -- 0 for false, 1 for true
+        completion_date DATE
+);
+
+CREATE TABLE habits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT NOT NULL,
+    created_date DATE NOT NULL,
+    periodicity TEXT NOT NULL -- e.g., 'daily', 'weekly', 'monthly'
+);
 
 CREATE TABLE tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,7 +33,9 @@ CREATE TABLE tasks (
     priority INTEGER,
     give_up BOOLEAN NOT NULL DEFAULT 0, -- 0 for false, 1 for true
     created_date DATE NOT NULL,
-    FOREIGN KEY (goal_id) REFERENCES goals(id)
+    habit_id INTEGER,
+    FOREIGN KEY (goal_id) REFERENCES goals(id),
+    FOREIGN KEY (habit_id) REFERENCES habits(id)
 );
 
 CREATE TABLE task_actions (
@@ -24,22 +46,13 @@ CREATE TABLE task_actions (
     FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
 
-CREATE TABLE goals (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        description TEXT NOT NULL,
-        created_date DATE NOT NULL,
-        target_date DATE NOT NULL,
-        completed BOOLEAN NOT NULL DEFAULT 0, -- 0 for false, 1 for true
-        completion_date DATE
-);
-
 CREATE TABLE notes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     note TEXT NOT NULL,
     type TEXT,
     created_date DATE NOT NULL
-)
+);
 
 CREATE TABLE random_things_to_do (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -47,4 +60,4 @@ CREATE TABLE random_things_to_do (
     completed BOOLEAN NOT NULL DEFAULT 0, -- 0 for false, 1 for true
     completion_date DATE,
     link TEXT
-)
+);
